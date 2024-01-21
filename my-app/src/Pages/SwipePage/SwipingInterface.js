2e10b979cef6baf520c11389c03bcd66c1d8e432
Jeremy Qiao <jeremy.qiao@gmail.com>
2024-01-20 20:27:43 -0800

    more card formatting

--- my-app/src/Pages/SwipePage/SwipingInterface.js
+++ my-app/src/Pages/SwipePage/SwipingInterface.js
@@ -8,7 +8,7 @@ const db = [
         name: "Green Acres",
         address: "123 Farm Road, Anytown, BC, Canada",
         rating: 4.9,
-        produceType: "potato",
+        produceType: "Potatoes",
         weight: 20,
         url:'./potatoes.png',
 
@@ -17,7 +17,7 @@ const db = [
         name: "Farmland 21",
         address: "201 Farm District, BC, Canada",
         rating: 4.0,
-        produceType: "onion",
+        produceType: "Onions",
         weight: 8,
         url:'./onions.png',
     },
@@ -25,7 +25,7 @@ const db = [
         name: "Farmers Market",
         address: "Somewhere",
         rating: 3.6,
-        produceType: "carrot",
+        produceType: "Carrots",
         weight: 12,
         url:'./carrots.png',
     },
@@ -37,7 +37,7 @@ function SwipingInterface() {
   
   
   {/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/}
-  const characters = db
+  const cards = db
   const [lastDirection, setLastDirection] = useState()
   const [curIndex, setCurIndex] = useState(0)
   const [swipeRight, setSwipeRight] = useState(false)
@@ -62,19 +62,22 @@ function SwipingInterface() {
     console.log(name + ' left the screen!')
   }
 
-//   var img1 = new Image();
-//   img1.src = "../assets/carrots.jpg";
-  console.log('url(\'' + characters[0].url + '\')')
 
   return (
     <div>
       <h1>Swipe Below</h1>
       <div className='cardContainer'>
-        {characters.map((character) =>
-          <TinderCard className='swipe' key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
+        {cards.map((card) =>
+          <TinderCard className='swipe' key={card.name} onSwipe={(dir) => swiped(dir, card.name)} onCardLeftScreen={() => outOfFrame(card.name)}>
             <div className='card'>
-              <img className="producePicture" src={character.url}></img>
-              <h3>{character.name}</h3>
+              <img className="producePicture" src={card.url}></img>
+              <div className='cardDescription'>
+                <h3>{card.name}</h3>
+                <p>{card.rating} / 5.0</p>
+                <p>{card.produceType}</p>
+                <p>{card.weight} lbs bag</p>
+              </div>  
+              
             </div>
           </TinderCard>
         )}
